cb456f8535c3777b9382178a4a1494fdc594c57c
DrCBeatz <brynecarruthers@gmail.com>
2024-08-04 18:21:40 -0400

    add ec2_deploy.sh

---
+++ ec2_deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install Docker
+sudo yum -y install docker
+
+# Start Docker service
+sudo service docker start
+
+# Enable Docker service to start on boot
+sudo systemctl enable docker
+
+# Add ec2-user to the docker group
+sudo usermod -a -G docker ec2-user
+
+# Give ec2-user permission to access Docker socket
+sudo chmod 666 /var/run/docker.sock
+
+# Install Git
+sudo yum install git -y
+
+# Check Git version
+git --version
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Pull the latest Docker image and start the app
+docker-compose up -d --build
+
+# Pull the specific model for your application
+docker-compose exec ollama ollama pull llama3.1:8b
